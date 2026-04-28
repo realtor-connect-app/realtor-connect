c291fbb578faef45ffad5ac0cd183058729c6350
Yaroslav Kotyk <makurohashami@gmail.com>
2026-04-28 19:13:28 +0200

    Small fixes

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM eclipse-temurin:17-jdk AS build
+FROM eclipse-temurin:21-jre AS build
 
 ARG MODULE
 
--- realtor-connect-server/src/main/java/com/makurohashami/realtorconnect/service/file/placeholder/PlaceholderFileService.java
+++ realtor-connect-server/src/main/java/com/makurohashami/realtorconnect/service/file/placeholder/PlaceholderFileService.java
@@ -2,6 +2,8 @@ package com.makurohashami.realtorconnect.service.file.placeholder;
 
 import com.makurohashami.realtorconnect.dto.file.FileUploadResponse;
 import com.makurohashami.realtorconnect.service.file.FileService;
+import io.micrometer.core.annotation.Counted;
+import io.micrometer.core.annotation.Timed;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Conditional;
@@ -18,15 +20,21 @@ public class PlaceholderFileService implements FileService {
     private String defaultAvatarUrl;
 
     @Override
+    @Counted(value = "realtorconnect.file.service")
+    @Timed(value = "realtorconnect.file.service", histogram = true)
     public FileUploadResponse uploadFile(MultipartFile file, Map<String, Object> params) {
         return FileUploadResponse.builder().url(defaultAvatarUrl).build();
     }
 
     @Override
+    @Counted(value = "realtorconnect.file.service")
+    @Timed(value = "realtorconnect.file.service", histogram = true)
     public void deleteFile(String path) {
     }
 
     @Override
+    @Counted(value = "realtorconnect.file.service")
+    @Timed(value = "realtorconnect.file.service", histogram = true)
     public void deleteFolder(String folder) {
     }
 
